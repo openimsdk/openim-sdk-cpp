ba502b29c666dbdeeda9be1e0c1384b1e497b1df
Gordon <1432970085@qq.com>
2023-09-03 18:08:08 +0800

    fix: modify the way of communication from C to Go.

--- c_wrapper/protocol.go
+++ c_wrapper/protocol.go
@@ -6,17 +6,14 @@ typedef void (*CB_I_S)(int,char *);
 typedef void (*CB_I_S_S)(int,char *,char *);
 typedef void (*CB_I_S_S_I)(int,char *,char *,int);
 
-__attribute__((weak))
 void Call_CB_I_S(CB_I_S func,int event,char* data)
 {
     func(event,data);
 }
-__attribute__((weak))
 void Call_CB_I_S_S(CB_I_S_S func,int errCode,char* errMsg,char* data)
 {
     func(errCode,errMsg,data);
 }
-__attribute__((weak))
 void Call_CB_I_S_S_I(CB_I_S_S_I func,int errCode,char* errMsg,char* data,int progress)
 {
     func(errCode,errMsg,data,progress);
