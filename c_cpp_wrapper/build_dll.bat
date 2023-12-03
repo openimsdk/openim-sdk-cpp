276e2909d73d18ef59d5aa2fdaef187fbd84feab
cncsmonster <cncsmonster@163.com>
2023-12-03 21:30:46 +0800

    feat: add sh for compile dll from cpp wrapper

--- c_cpp_wrapper/build_dll.bat
+++ c_cpp_wrapper/build_dll.bat
@@ -1,2 +1,5 @@
 go build -buildmode=c-shared  -trimpath -ldflags="-s -w"  -o openimsdk.dll  export.go constant.go protocol.go tools.go
 
+
+
+g++ -shared -fPIC -o openimsdkcc.dll openimsdkcc.cc openimsdk.dll
