74b8ebe9c23ef297c31a2388fcf1c5a6dc346beb
yejian <596306006@qq.com>
2024-02-18 15:37:25 +0800

    dev: add cmake env

---
+++ examples/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "IMSDK.h"
+using namespace OpenIM;
+
+int main()
+{
+    InitSDK();
+    std::cout << "helloworld" << std::endl;
+    return 0;
+}
---
+++ src/IMSDK.cpp
@@ -0,0 +1,20 @@
+#include "IMSDK.h"
+
+#include <iostream>
+#include <sstream>
+#include "json.hpp"
+using json = nlohmann::json;
+char *GetOperationId(const char *prefix)
+{
+    static int operationIndex = 0;
+    operationIndex = operationIndex + 1;
+    std::stringstream ss;
+    ss << prefix << "_" << operationIndex;
+    return (char *)ss.str().c_str();
+}
+
+void OpenIM::InitSDK()
+{
+       
+    init_sdk(GetOperationId("init_sdk"), "");
+}
