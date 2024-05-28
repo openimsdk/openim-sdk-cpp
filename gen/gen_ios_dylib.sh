fd157b1719763b932ca7c079f08e405274a8836b
yejian <596306006@qq.com>
2024-05-28 08:37:36 +0800

    gen_ios_dylib.sh

--- gen/gen_ios_dylib.sh
+++ gen/gen_ios_dylib.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 export LANG=en_US.UTF-8
-
+# ios device
 export CFLAGS="-arch arm64 -miphoneos-version-min=9.0 -isysroot "$(xcrun -sdk iphoneos --show-sdk-path) 
 export CGO_LDFLAGS="-arch arm64 -miphoneos-version-min=9.0 -isysroot "$(xcrun -sdk iphoneos --show-sdk-path)  
 export CGO_ENABLED=1 
 export GOARCH=arm64 
 export GOOS=darwin 
 export CC="clang $CFLAGS $CGO_LDFLAGS" 
-go build -tags ios -ldflags=-w -trimpath -v -o libopenimsdk_ios.a -buildmode c-archive export.go protocol.go message.go
+go build -tags ios -ldflags=-w -trimpath -v -o libopenimsdk.a -buildmode c-archive export.go protocol.go message.go
+# go build -tags ios -ldflags=-w -trimpath -v -o libopenimsdk_ios.a -buildmode c-archive export.go protocol.go message.go
+# ios simulator
+# export CFLAGS="-arch x86_64 -miphoneos-version-min=9.0 -isysroot "$(xcrun -sdk iphonesimulator --show-sdk-path) 
+# export CGO_LDFLAGS="-arch x86_64 -miphoneos-version-min=9.0 -isysroot "$(xcrun -sdk iphonesimulator --show-sdk-path) 
+# CGO_ENABLED=1
+# GOARCH=amd64 
+# GOOS=darwin 
+# CC="clang $CFLAGS $CGO_LDFLAGS" 
+# go build -tags ios -ldflags=-w -trimpath -v -o libopenimsdk_iossimulator.a -buildmode c-archive
  
-export CFLAGS="-arch x86_64 -miphoneos-version-min=9.0 -isysroot "$(xcrun -sdk iphonesimulator --show-sdk-path) 
-export CGO_LDFLAGS="-arch x86_64 -miphoneos-version-min=9.0 -isysroot "$(xcrun -sdk iphonesimulator --show-sdk-path) 
-CGO_ENABLED=1
-GOARCH=amd64 
-GOOS=darwin 
-CC="clang $CFLAGS $CGO_LDFLAGS" 
-go build -tags ios -ldflags=-w -trimpath -v -o libopenimsdk_iossimulator.a -buildmode c-archive
- 
-lipo -create libopenimsdk_ios.a libopenimsdk_iossimulator.a -output libopenimsdk.a
-# lipo -info ${baseDir}/test.a
+# lipo -create libopenimsdk_ios.a libopenimsdk_iossimulator.a -output libopenimsdk.a
